45add36ab8b493040ee7ed65008e08394732475c
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-17 18:31:24 +0800

    Refactor

--- src/Console/EnvSailCommand.php
+++ src/Console/EnvSailCommand.php
@@ -5,7 +5,6 @@ namespace Jedymatt\LaravelEnvSail\Console;
 use Laravel\Sail\Console\InstallCommand;
 use Symfony\Component\Yaml\Yaml;
 
-
 class EnvSailCommand extends InstallCommand
 {
     /**
@@ -23,6 +22,23 @@ class EnvSailCommand extends InstallCommand
     protected $description = 'Configure the environment variables for the application';
 
     /**
+     * Sail services
+     *
+     * @var string[]
+     */
+    protected $services = [
+        'mysql',
+        'pgsql',
+        'mariadb',
+        'redis',
+        'memcached',
+        'meilisearch',
+        'minio',
+        'mailhog',
+        'selenium',
+    ];
+
+    /**
      * Execute the console command.
      *
      * @return int
@@ -30,43 +46,32 @@ class EnvSailCommand extends InstallCommand
     public function handle()
     {
         // Create .env file if it doesn't exist
-        if (!file_exists($this->laravel->basePath('.env'))) {
+        if (! file_exists($this->laravel->basePath('.env'))) {
             $this->warn('No .env file found. Creating .env file from .env.example');
             copy($this->laravel->basePath('.env.example'), $this->laravel->basePath('.env'));
         }
 
-        if (!file_exists($this->laravel->basePath('docker-compose.yml'))) {
-            $this->error('docker-compose.yml not found. Please run "sail:install" first.');
+        if (! file_exists($this->laravel->basePath('docker-compose.yml'))) {
+            $this->error('docker-compose.yml not found. Please run "php artisan sail:install" first.');
+
             return 1;
         }
 
-        $services = $this->getSailServices();
+        $services = $this->servicesFromDockerComposeFile();
 
-        $this->comment('Service(s) detected from docker-compose.yml: ' . implode(', ', $services));
+        $this->comment('Service(s) detected from docker-compose.yml: ['.implode(',', $services).']');
 
         $this->replaceEnvVariables($services);
 
         $this->info('Successfully configured .env file.');
     }
 
-    protected function getSailServices(): array
+    protected function servicesFromDockerComposeFile(): array
     {
         $dockerCompose = Yaml::parseFile($this->laravel->basePath('docker-compose.yml'));
 
-        $services = [
-            'mysql',
-            'pgsql',
-            'mariadb',
-            'redis',
-            'memcached',
-            'meilisearch',
-            'minio',
-            'mailhog',
-            'selenium',
-        ];
-
-        $sailServices = array_filter($dockerCompose['services'], function ($service) use ($services) {
-            return in_array($service, $services);
+        $sailServices = array_filter($dockerCompose['services'], function ($service) {
+            return in_array($service, $this->services);
         }, ARRAY_FILTER_USE_KEY);
 
         return array_keys($sailServices);
